7becb84c98d1ae90994c451545a19fd1800e2c50
okekejessica <jessicaokeke50@gmail.com>
2024-10-30 06:17:53 +0100

    Moved images

--- src/Advice.jsx
+++ src/Advice.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import divider from "../src/assets/pattern-divider-desktop.svg";
+import dice from "../src/assets/icon-dice.svg";
 import { ClipLoader } from "react-spinners";
 
 const Advice = ({ advice, getAdvice, loading }) => {
@@ -13,15 +15,11 @@ const Advice = ({ advice, getAdvice, loading }) => {
         <>
           <h1 className="title mb-0">ADVICE #{advice.id}</h1>
           <p className="mb-2 advice">"{advice.advice}"</p>
-          <img
-            className="desk-img "
-            src="/src/assets/pattern-divider-desktop.svg"
-            alt=""
-          />
+          <img className="desk-img " src={divider} alt="" />
 
           <button onClick={getAdvice}>
             <div className="dice ">
-              <img id="advice-button" src="/src/assets/icon-dice.svg" alt="" />
+              <img id="advice-button" src={dice} alt="" />
             </div>
           </button>
         </>
